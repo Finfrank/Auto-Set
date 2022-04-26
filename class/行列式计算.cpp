177e9dc42b8e7103014c4dabbc8b252f64dd779a
Finfrank <lfh142857@gmail.com>
2022-04-26 17:34:42 +0800

    行列式

--- class/行列式计算.cpp
+++ class/行列式计算.cpp
@@ -15,60 +15,39 @@ using namespace std;
 long long a[505][505];
 
 int main() {
-
-int n; 
-long long p; 
-bool key = 0; 
-long long ans = 1; 
-cin >> n >> p;
-
-for (int i = 1; i <= n; i++)
-for (int j = 1; j <= n; j++) {
-cin >> a[i][j];
-
-a[i][j] = a[i][j] % p;
-
-}
-
-for (int i = 1; i <= n; i++) {
-
-for (int j = i + 1; j <= n; j++) {
-
-int x = i;
-
-int y = j;
-
-long long t = a[x][i] / a[y][i];
-
-while (a[y][i]) { for (int k = 1; k <= n; k++)
-
-a[x][k] = (a[x][k] - t * a[y][k]) % p; swap(x, y);
-
-} if (x != i) {
-
-key ^= 1;
-
-for (int k = 1; k <= n; k++)
-
-swap(a[i][k], a[x][k]);
-
-}
-
-}
-
-ans *= a[i][i];
-
-} if (key)
-
-ans = ans * (-1);
-
-ans = ans % p;
-
-ans += p;
-
-ans = ans % p;
-
-cout << ans << endl; 
-return 0;
-
+	int n;
+	long long p;
+	bool key = 0;
+	long long ans = 1;
+	cin >> n >> p;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++) {
+			cin >> a[i][j];
+			a[i][j] = a[i][j] % p;
+		}
+	for (int i = 1; i <= n; i++) {
+		for (int j = i + 1; j <= n; j++) {
+			int x = i;
+			int y = j;
+			while (a[y][i]) {
+				long long t = a[x][i] / a[y][i];
+				for (int k = 1; k <= n; k++)
+					a[x][k] = (a[x][k] - t * a[y][k]) % p;
+				swap(x, y);
+			}
+			if (x != i) {
+				key ^= 1;
+				for (int k = 1; k <= n; k++)
+					swap(a[i][k], a[x][k]);
+			}
+		}
+		ans *= a[i][i];
+	}
+	if (key)
+		ans = ans * (-1);
+	ans = ans % p;
+	ans += p;
+	ans = ans % p;
+	cout << ans << endl;
+	return 0;
 }
